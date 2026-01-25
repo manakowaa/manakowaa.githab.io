7dbd0baad2e299940c25cb277ecba4eed5da2a58
manakowaa <manakowa.nika@yandex.ru>
2026-01-25 16:49:12 +0300

    first commit

--- SRC/scripts/dev/jquery.validate-init.js
+++ SRC/scripts/dev/jquery.validate-init.js
@@ -49,5 +49,36 @@ if (eventForm.length) {
     errorElement: "span"
   });
 }
+
+const subscribeForm = $("#js-subscribeForm");
+
+if (subscribeForm.length) {
+
+  const subscribeAction = subscribeForm.attr("action");
+  const subscribeEmail = subscribeForm.find("#js-subscribeEmail");
+
+  subscribeForm.validate({
+    errorElement: "span",
+    submitHandler: function (form, event) {
+      event.preventDefault();
+
+      $.ajax({
+        url: subscribeAction,
+        method: "POST",
+        data: {
+          email: subscribeEmail.val()
+        },
+        success: function () {
+          subscribeEmail.val("");
+          subscribeEmail.blur();
+          alert("Вы успешно подписались на рассылку новостей");
+        },
+        error: function () {
+          alert("Что-то пошло не так, попробуйте еще раз");
+        }
+      });
+    }
+  });
+}
         
 })();
